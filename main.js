da9165f038afb983a3c48479b2ce881b53719384
Theonlyhamstertoh <theonlyhamstertoh@gmail.com>
2021-02-18 12:42:16 -0800

    changed circles color, fix unmoving circle bug

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
-const canvas = document.getElementById('tutorial');
+const canvas = document.getElementById('canvas');
+
+//made canvas size to max viewport
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let ctx = canvas.getContext('2d');
+// 'context' is shorten to 'ctx'
+const ctx = canvas.getContext('2d');
 
 
+// we want to move the object and to move a object, we need to increment the x and y and clear the previous so it can have the illusion that it is moving forward when it really is not. 
 
-let colorArray = [ 
-    '#7FB069',
-    '#FFFBBD',
-    '#E6AA68',
-    '#CA3C25',
-    '#1D1A05'];
+let colorArray = [
+    '#45425A',
+    '#575C55',
+    '#6C7D47',
+    '#96A13A',
+    '#ACC12F'] 
 
-
-
-let mouse = {
-    x: undefined,
-    y: undefined
+let circleArray = [];
+function initialize() {
+    circleArray = [];
+    for(let i = 0; i < 600; i++) {
+        let radius = Math.round(Math.random() * 20) + 5;
+        let x = Math.round(Math.random() * (innerWidth - (2 * radius))) + radius;
+        let y = Math.round(Math.random() * (innerHeight - (2 * radius))) + radius;
+        let dx = Math.round(Math.random() * 5) + 1;
+        let dy = Math.round(Math.random() * 5) + 1;
+        let circle = new Circle(x, y, dx, dy, radius);
+        circleArray.push(circle);
+    }
 }
 
 let maxRadius = 60;
 let minRadius = 2;
-window.addEventListener('mousemove', function(e) {
-    mouse.x = e.x;
-    mouse.y = e.y;
-    console.log(mouse.x, mouse.y);
-})
-let circleArray = [];
-initialize();
-function initialize() {
-    circleArray = [];
-    for(let i = 0; i < 600; i++) {   
-        let radius = Math.round(Math.random() * 10) + 5;
-        let x = Math.round(Math.random() * (innerWidth - (radius * 2)) + radius);
-        let y = Math.round(Math.random() * (innerHeight- (radius * 2)) + radius);
-        let speedX = Math.round(Math.random() * 3);
-        let speedY =  Math.round(Math.random() * 3);
-        circleArray.push(new Circle(x, y, speedX, speedY, radius))
-        
-    }
+let mouse = {
+    x: undefined,
+    y: undefined
 }
 
 window.addEventListener('resize', function() {
@@ -49,48 +45,42 @@ window.addEventListener('resize', function() {
     initialize();
 })
 
-function Circle(x, y, speedX, speedY, radius) {
-    console.log('passed')
+window.addEventListener('mousemove', function(e) {
+    mouse.x = e.x;
+    mouse.y = e.y;
+    console.log(mouse.x, mouse.y);
+})
+
+initialize();
+function Circle(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
-    this.speedX = speedX;
-    this.speedY = speedY;
+    this.dx = dx;
+    this.dy = dy;
     this.radius = radius;
     this.value = this.radius;
     this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
- 
-    
-    
 
-    //Creates a circle and color it
-    this.draw = () => {
+
+    this.draw = function() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.arc(this.x, this.y, this.radius , 0, Math.PI * 2, false);
         ctx.fillStyle = this.color;
         ctx.fill();
-       
+        
     }
-
-    //allows the circle to bounce on window border and update it
-    this.update = () => {
-        if(this.x + this.radius> innerWidth || this.x - this.radius < 0) {
-            //changes direction 
-            this.speedX = -this.speedX;
-            // changes color
-            ctx.fill();
-            
+    this.update = function() {
+        this.x += this.dx;
+        this.y += this.dy;
+        
+        if(this.x + this.radius > window.innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
         }
-        if(this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-            this.speedY = -this.speedY;
-            ctx.fill();
+        if(this.y + this.radius > window.innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
         }
 
-        //increases speed
-        this.x += this.speedX;
-        this.y += this.speedY;
-
-        //interactivity
-        if (Math.abs(mouse.x - this.x) < 100 && Math.abs(mouse.y - this.y) < 100) {
+        if(Math.abs(mouse.x - this.x) < 100 && Math.abs(mouse.y - this.y) < 100) {
             if(this.radius <= maxRadius){
                 this.radius += 1;
         }
@@ -99,19 +89,25 @@ function Circle(x, y, speedX, speedY, radius) {
 
         }
 
+       
         this.draw();
     }
+
 }
+
 
 animate();
 function animate() {
+    console.log('it\'s working')
     ctx.clearRect(0, 0, innerWidth, innerHeight);
     requestAnimationFrame(animate);
 
     for(let i = 0; i < circleArray.length; i++) {
         circleArray[i].update();
-        
     }
 
-   
+
 }
+
+
+
